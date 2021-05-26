c836db49f0d2c4d1cd85555b2f005628942ae9a2
fillipe-felix <felipesoares_1993@hotmail.com>
2021-05-25 21:19:22 -0300

    criando a model de Infectado e a conexão com o Mongo

---
+++ dio_api_mongo_dotnet/Data/Collections/Infectado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MongoDB.Driver.GeoJsonObjectModel;
+
+namespace dio_api_mongo_dotnet.Data.Collections
+{
+    public class Infectado
+    {
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public GeoJson2DGeographicCoordinates Localizacao { get; set; }
+        
+        public Infectado(DateTime dataNascimento, string sexo, double latitude, double longitude)
+        {
+            this.DataNascimento = dataNascimento;
+            this.Sexo = sexo;
+            this.Localizacao = new GeoJson2DGeographicCoordinates(longitude, latitude);
+        }
+    }
+}
